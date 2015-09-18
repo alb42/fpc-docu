3c39eb07965b15ce34351c92345e599acbd92938
alb42 <alb42@web.de>
2015-09-18 19:01:48 +0100

    Start of MUI documentation, renamed AllocMem to ExecAllocMem as in the unit

--- FileList.sh
+++ FileList.sh
@@ -2,9 +2,10 @@
 export CURDIR=$(pwd)
 export AROSUNITSPATH=$CURDIR/arosunits/src
 
-export AROSUNITS="--input=$AROSUNITSPATH/hardware.pas \
-  --input=$AROSUNITSPATH/exec.pas --descr=$CURDIR/xml/exec.xml \
-  --input=$AROSUNITSPATH/utility.pas --descr=$CURDIR/xml/utility.xml  \
+export AROSUNITS="--css-file=$CURDIR/xml/fpdoc.css \
+  --input=$AROSUNITSPATH/hardware.pas \
+  --input=$AROSUNITSPATH/exec.pas     --descr=$CURDIR/xml/exec.xml \
+  --input=$AROSUNITSPATH/utility.pas  --descr=$CURDIR/xml/utility.xml  \
   --input=$AROSUNITSPATH/amigados.pas --descr=$CURDIR/xml/amigados.xml \
   --input=$AROSUNITSPATH/agraphics.pas --descr=$CURDIR/xml/agraphics.xml \
   --input=$AROSUNITSPATH/intuition.pas --descr=$CURDIR/xml/intuition.xml \
@@ -15,11 +16,11 @@ export AROSUNITS="--input=$AROSUNITSPATH/hardware.pas \
   --input=$AROSUNITSPATH/inputevent.pas \
   --input=$AROSUNITSPATH/asl.pas \
   --input=$AROSUNITSPATH/clipboard.pas \
-  --input=$AROSUNITSPATH/diskfont.pas --descr=$CURDIR/xml/diskfont.xml \
+  --input=$AROSUNITSPATH/diskfont.pas    --descr=$CURDIR/xml/diskfont.xml \
   --input=$AROSUNITSPATH/gadtools.pas \
   --input=$AROSUNITSPATH/iffparse.pas \
   --input=$AROSUNITSPATH/keymap.pas \
-  --input=$AROSUNITSPATH/mui.pas \
+  --input=$AROSUNITSPATH/mui.pas         --descr=$CURDIR/xml/mui.xml \
   --input=$AROSUNITSPATH/clipboard.pas \
-  --input=$AROSUNITSPATH/workbench.pas --descr=$CURDIR/xml/workbench.xml \
-  --input=$AROSUNITSPATH/icon.pas --descr=$CURDIR/xml/icon.xml"
+  --input=$AROSUNITSPATH/workbench.pas   --descr=$CURDIR/xml/workbench.xml \
+  --input=$AROSUNITSPATH/icon.pas        --descr=$CURDIR/xml/icon.xml"
